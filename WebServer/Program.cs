ac7af6f89a7fe01e4358e0efad9dbe0d07cca31c
abaga129 <ethanepr@hotmail.com>
2016-12-06 19:50:39 -0600

    Major Fixes

--- WebServer/Program.cs
+++ WebServer/Program.cs
@@ -12,7 +12,8 @@ namespace WebServer
     class Program
     {
         private static Socket Server;
-
+        private static string html = "<head></head><body><html><h1>Welcome to the CS480\\Demo Server</h1><p>Why not visit: <ul><li><a href =\"http://www2.semo.edu/csdept/ \">Computer Science Home Page</a><li><a href =\"http://cstl-csm.semo.edu/liu/cs480_fall2012/index.htm\"\\>CS480 Home Page<a></ul></html></body>\n";
+        private static SocketFlags flags;
         static void Main(string[] args)
         {
             AddMessage("Starting Server...");
@@ -26,15 +27,22 @@ namespace WebServer
                 {
                     byte[] data = new byte[1024];
                     int dataLength = client.Receive(data);
-
                     if (dataLength == 0) break;
+                    string message = Encoding.ASCII.GetString(data, 0, dataLength);
+                    AddMessage(message);
 
-                    AddMessage(Encoding.ASCII.GetString(data, 0, dataLength));
+                    if (message.Contains("\r\n\r\n")) ;
+                    AddMessage("Header Fully Recieved");
+                    break;
 
-                    string message = "Test\n";
-                    data = Encoding.ASCII.GetBytes(message);
-                    client.Send(data, SocketFlags.None);
+                    //string message = "Test\n";
+                    //data = Encoding.ASCII.GetBytes(message);
+                    //client.Send(data, SocketFlags.None);
                 }
+                SendHead(client, 200, html.Length);
+                AddMessage("Sending: " + html);
+                byte[] htmlData = Encoding.ASCII.GetBytes(html + "\r\n\r\n");
+                client.Send(htmlData, SocketFlags.None);
 
                 DisconnectFromClient(client);
 
@@ -95,6 +103,51 @@ namespace WebServer
             bool quitting = Console.ReadLine().ToLower().Contains("y");
             if (quitting) Console.WriteLine("The server is shutting down...");
             return quitting;
+        }
+
+        private static void SendHead(Socket conn, int stat, int len)
+        {
+            String statStr = "";
+            switch (stat)
+            {
+                case 200:
+                    statStr = "OK";
+                    break;
+                case 400:
+                    statStr = "Bad Request";
+                    break;
+                case 404:
+                    statStr = "Not Found";
+                    break;
+                default:
+                    statStr = "Unknown";
+                    break;
+            }
+
+            string message = "HTTP/1.1 " + stat + " " + statStr + "\r\n";
+            byte[] data = Encoding.ASCII.GetBytes(message);
+            AddMessage(message);
+            conn.Send(data, SocketFlags.None);
+
+            message = "Server: CS480 Demo Server\r\n";
+            data = Encoding.ASCII.GetBytes(message);
+            AddMessage(message);
+            conn.Send(data, SocketFlags.None);
+
+            message = "Content-Length: " + len.ToString() + "\r\n";
+            data = Encoding.ASCII.GetBytes(message);
+            AddMessage(message);
+            conn.Send(data, SocketFlags.None);
+
+            message = "Content-Type: text/html\r\n";
+            data = Encoding.ASCII.GetBytes(message);
+            AddMessage(message);
+            conn.Send(data, SocketFlags.None);
+
+            message = "\r\n";
+            data = Encoding.ASCII.GetBytes(message);
+            AddMessage(message);
+            conn.Send(data, SocketFlags.None);
         }
     }
 }
